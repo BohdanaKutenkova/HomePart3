70e9005efa030ee8fd7c721c6d325f118968e6fe
Bohdana Kutenkova <bohdanakutenkova@gmail.com>
2022-09-14 10:30:37 +0300

    changed

--- src/main/java/LambdaMetod/Lambda1.java
+++ src/main/java/LambdaMetod/Lambda1.java
@@ -10,12 +10,12 @@ public class Lambda1 {
         SumArray obj1 = new SumArray();
         int[] A = { 7, 8, 11, 6, 2, 8, 5, 9 };
         int sum = obj1.Sum(A, ref1);
-        System.out.println("sum = " + sum);
+        System.out.println("sum Evens = " + sum);
         sum = obj1.Sum(A,
                 (Odd)((d) -> {
                     if (d%2!=0) return true;
                     else return false;
                 }));
-        System.out.println("sum = " + sum);
+        System.out.println("sum Odds = " + sum);
     }
 }
